52fff065f7699c7378d6f6bd97a8c9925e86fbbf
Brenden Romanowski <brenden.romanowski@gmail.com>
2019-01-15 10:51:03 -0500

    Adding nodes to sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,9 @@ values(2, 'O=PartyB, L=London, C=GB', 'T-Mobile', FALSE );
 
 insert into node_identity(id, cordaX500Name, nickname, is_parent_company)
 values(3, 'O=PartyC, L=London, C=GB', 'AT&T', FALSE );
+
+insert into node_identity(id, cordaX500Name, nickname, is_parent_company)
+values(4, 'O=PartyD, L=London, C=GB', 'Sprint', FALSE );
+
+insert into node_identity(id, cordaX500Name, nickname, is_parent_company)
+values(5, 'O=PartyE, L=London, C=GB', 'U.S. Cellular', FALSE );
