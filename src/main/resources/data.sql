302a132303cd95f433208576451911bd73b72287
Brenden Romanowski <brenden.romanowski@gmail.com>
2020-03-18 17:05:44 -0500

    Updating party names

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,13 +2,13 @@ insert into node_identity(id, cordaX500Name, nickname, is_parent_company)
 values(1, 'O=ParentCompany, L=London, C=GB', 'Verizon Wireless', TRUE );
 
 insert into node_identity(id, cordaX500Name, nickname, is_parent_company)
-values(2, 'O=PartyB, L=London, C=GB', 'T-Mobile', FALSE );
+values(2, 'O=PartyA, L=London, C=GB', 'T-Mobile', FALSE );
 
 insert into node_identity(id, cordaX500Name, nickname, is_parent_company)
-values(3, 'O=PartyC, L=London, C=GB', 'AT&T', FALSE );
+values(3, 'O=PartyB, L=London, C=GB', 'AT&T', FALSE );
 
 insert into node_identity(id, cordaX500Name, nickname, is_parent_company)
-values(4, 'O=PartyD, L=London, C=GB', 'Sprint', FALSE );
+values(4, 'O=PartyC, L=London, C=GB', 'Sprint', FALSE );
 
 insert into node_identity(id, cordaX500Name, nickname, is_parent_company)
-values(5, 'O=PartyE, L=London, C=GB', 'U.S. Cellular', FALSE );
+values(5, 'O=PartyD, L=London, C=GB', 'U.S. Cellular', FALSE );
